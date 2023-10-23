1c8a01abf18823c0e1dc4d66e5105848b0b6be24
FacuStoffel <stoffelfacu29@gmail.com>
2023-10-22 23:17:27 -0300

    roles en menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,100 +2,106 @@ import { Link, useNavigate } from 'react-router-dom';
 import './styles/index.css';
 import jwt_decode from "jwt-decode";
 
-function Menu()  {
+function Menu() {
 
   const navigate = useNavigate();
 
-  function logout()  {
+  function logout() {
     sessionStorage.removeItem('token');
-navigate("/");
-    // navigate("/");
+    navigate("/");
   }
 
 
 
-    const token = sessionStorage.getItem('token');
+  const token = sessionStorage.getItem('token');
+  // var tokenDecoded = jwt_decode(sessionStorage.getItem('token'));
+  // const rol = tokenDecoded.rol;
+  // console.log(rol)
 
 
 
 
-    if (token) {
-      var decoded = jwt_decode(token);
-      return (
-        <>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary container-fluid ">
-            <div className="container-fluid ">
-              <a class="navbar-brand " href="/">
-                <img src="./logo.png" class="logo" alt="Logo" width="50" height="50" />
-              </a>
-              <a className="navbar-brand letra" href="/">Barberia</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link active letra">Inicio</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/reservas" className="nav-link active letra">Reservas</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/horario" className="nav-link active letra">Horarios</Link>
-                  </li>
-                </ul>
-              </div>
-              {decoded.mail}
-
-              <button type="button" className='btn btn-danger' onClick={() => logout()}>
-                <span className="material-symbols-outlined">
-                  logout
-                </span>
-              </button>
-
-            </div>
-          </nav>
-        </>
-      )
-    } else {
-      return (
-        <>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary container-fluid ">
-            <div className="container-fluid ">
-              <a class="navbar-brand " href="/">
-                <img src="./logo.png" class="logo" alt="Logo" width="50" height="50" />
-              </a>
-              <a className="navbar-brand letra" href="/">Barberia</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item">
-                    <Link to="/" className="nav-link active letra">Inicio</Link>
-                  </li>
-                  {/* <li className="nav-item">
-                  <Link to="/reservas" className="nav-link active letra">Reservas</Link>
-                </li> */}
-                </ul>
-              </div>
-              <ul className="nav justify-content-end boton">
-                <li className="nav-item " >
-                  <Link to="/login" className="nav-link letra sesion">
-                    <button type="button" className="btn btn-outline-primary ">Iniciar Sesión</button></Link>
+  if (token) {
+    var decoded = jwt_decode(token);
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary container-fluid ">
+          <div className="container-fluid ">
+            <a class="navbar-brand " href="/">
+              <img src="./logo.png" class="logo" alt="Logo" width="50" height="50" />
+            </a>
+            <a className="navbar-brand letra" href="/">Barberia</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active letra">Inicio</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to="/register" className="nav-link letra sesion">
-                    <button className="btn btn-primary" type="submit">Registrase</button></Link>
+                  <Link to="/reservas" className="nav-link active letra">Reservas</Link>
+                </li>
+                <li className="nav-item">
+                  {decoded.rol === "Admin"
+                    ? <Link to="/horario" className="nav-link active letra">Horarios</Link>
+                    : null
+                  }
+
                 </li>
               </ul>
             </div>
-          </nav>
-        </>
-      )
-    }
+            {decoded.mail}
 
+            <button type="button" className='btn btn-danger' onClick={() => logout()}>
+              <span className="material-symbols-outlined">
+                logout
+              </span>
+            </button>
+
+          </div>
+        </nav>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary container-fluid ">
+          <div className="container-fluid ">
+            <a class="navbar-brand " href="/">
+              <img src="./logo.png" class="logo" alt="Logo" width="50" height="50" />
+            </a>
+            <a className="navbar-brand letra" href="/">Barberia</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active letra">Inicio</Link>
+                </li>
+                {/* <li className="nav-item">
+                  <Link to="/reservas" className="nav-link active letra">Reservas</Link>
+                </li> */}
+              </ul>
+            </div>
+            <ul className="nav justify-content-end boton">
+              <li className="nav-item " >
+                <Link to="/login" className="nav-link letra sesion">
+                  <button type="button" className="btn btn-outline-primary ">Iniciar Sesión</button></Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/register" className="nav-link letra sesion">
+                  <button className="btn btn-primary" type="submit">Registrase</button></Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
+    )
   }
+
+}
 
 
 export default Menu
